56b4571cacf55f1944cf5acef8c58de96de80f0c
Katja-Schroeder@web.de <Katja-Schroeder@web.de>
2021-02-19 12:26:48 +0100

    make it save

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :cocktail, uniqueness: { scope: :ingredient, message: "Please choose another dose!" }
   validates :description, presence: true
 end

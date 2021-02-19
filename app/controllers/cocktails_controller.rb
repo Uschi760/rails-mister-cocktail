49b3191aea24d5bf97f2e678c4b774731dc40c1e
Katja-Schroeder@web.de <Katja-Schroeder@web.de>
2021-02-19 16:20:47 +0100

    cocktail-controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -12,3 +12,38 @@ end
 
 # GET /cocktails/1
 def show; end
+
+def create
+  @cocktail = Cocktail.new(pet_params)
+  if @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  else
+    render 'new'
+  end
+end
+
+def edit; end
+
+def update
+  if @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  else
+    render 'edit'
+  end
+
+def destroy
+  @cocktail.destroy
+  redirect_to cocktail_path
+end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+end
